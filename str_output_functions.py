d4ffb491a0677211a2ee7a99ea052a83a8172e7e
Patrick D Faerber <patrickdfaerber@gmail.com>
2023-12-10 12:49:26 -0500

    Initial commit

---
+++ str_output_functions.py
@@ -0,0 +1,15 @@
+
+#Fun with python function string outputs
+
+#prints title case of name
+# f_name = input('What is your first name? \n')
+# l_name = input('What is your last name? \n')
+def format_name1(f_name, l_name):
+    name = f_name + " " + l_name
+    return print(name.title())
+format_name1("patrick", "faerber")
+
+def format_name2(f_name, l_name):
+    frmt_f_name = f_name.title()
+    frmt_l_name = l_name.title()
+    print(f'{frmt_f_name} {frmt_l_name}')
