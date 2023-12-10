fdda1091f8fa0e9a8ee3243251847afcdcd275a7
Patrick D Faerber <patrickdfaerber@gmail.com>
2023-12-10 14:50:31 -0500

    return an fstring with v3 of function and call in print statement

--- str_output_functions.py
+++ str_output_functions.py
@@ -14,3 +14,10 @@ def format_name2(f_name, l_name):
     frmt_l_name = l_name.title()
     print(f'{frmt_f_name} {frmt_l_name}')
 format_name2("paTRick", "fAErber")
+
+def format_name3(f_name, l_name):
+    frmt_f_name = f_name.title()
+    frmt_l_name = l_name.title()
+    return f'{frmt_f_name} {frmt_l_name}'
+
+print(format_name3("paTRICK", "fAErber"))
